5990f6d0e237967ce3afa1c720cd1ce4c0da5eaa
Berkay <berkay.ozcelikv@gmail.com>
2022-12-30 01:11:27 +0300

    Relase v3

---
+++ src/main/java/com/libx/LibXBackend/BookRepo.java
@@ -0,0 +1,50 @@
+package com.libx.LibXBackend;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.*;
+
+public class BookRepo {
+
+    public static ArrayList<String> allBookNames = new ArrayList<>();
+    public static ArrayList<Integer> allBookNumbers = new ArrayList<>();
+
+    static {
+        readAllDataAtOnce();
+    }
+
+    public static List<Object[]> getNumbersAndFullNames(String prefix) {
+        ArrayList<Object[]> found = new ArrayList<>();
+
+        int i = 0;
+        for (String bookName : allBookNames) {
+            if (bookName.startsWith(prefix)) {
+                Object[] pair = new Object[]{allBookNumbers.get(i), bookName};
+                found.add(pair);
+            }
+            i++;
+        }
+        return found;
+    }
+
+    private static void readAllDataAtOnce() {
+        try {
+            FileReader filereader = new FileReader("pg_catalog.csv");
+
+            CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+            List<String[]> allData = csvReader.readAll();
+
+            for (String[] row : allData) {
+                allBookNames.add(row[3]);
+                allBookNumbers.add(Integer.parseInt(row[0]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
--- src/main/java/com/libx/LibXBackend/controllers/BookController.java
+++ src/main/java/com/libx/LibXBackend/controllers/BookController.java
@@ -1,32 +1,23 @@
 package com.libx.LibXBackend.controllers;
 
-
-import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.HashMap;
+import java.util.List;
+
+import com.libx.LibXBackend.BookRepo;
 
 @RestController
 @RequestMapping("/book_api_v2")
 public class BookController {
     @GetMapping("/search/{bookName}")
-    public ResponseEntity<ArrayList<Object[]>> getBookNames(@PathVariable String bookName) {
-        ArrayList<Object[]> foundBooks = new ArrayList<>();
-
-        for (int bookID = 0; bookID < 5; bookID++) {
-            Object[] pair = {bookID, "Test book" + 1};
-
-            foundBooks.add(pair);
-        }
+    public ResponseEntity<List<Object[]>> getBookNames(@PathVariable String bookName) {
+        List<Object[]> foundBooks = BookRepo.getNumbersAndFullNames(bookName);
         return new ResponseEntity<>(foundBooks, HttpStatus.OK);
     }
 
